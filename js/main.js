27ddd8a905a39848e6d369960e1b7042503bc53e
John Stratton <jstrat0407@gmail.com>
2017-07-29 04:09:12 -0400

    Completed form using template literals

--- js/main.js
+++ js/main.js
@@ -1,34 +1,45 @@
 // build contact form
-let form = $('<form/>', {
-  action: ' ',
-  id: 'contact-us',
-  class: 'form-horizontal contact-form'
-});
-let formGroup = $('<div/>', {
-  class: 'form-group'
-});
-let controlLabel = `col-sm-2 control-label`;
-let colDiv = $('<div/>', {
-  class: 'col-sm-10'
-});
-//let formControl = `class="form-control"`;
-let name = $(formGroup).append(
-  $('<label/>', {
-    for: 'name',
-    class: controlLabel
-  }).append('Name:'),
-  $(colDiv).append(
-    $('<input>', {
-      type: 'text',
-      class: 'form-control',
-      id: 'name',
-      name: 'user_name',
-      placeholder: 'Your Name'
-    })
-  )
-);
-let contactForm = $(form).append(name);
+let form = `<form action=" " id="contact-us" class="form-horizontal contact-form">`;
+let formGroup = `<div class="form-group">`;
+let label = `<label for="`;
+let controlLabel = `class="col-sm-2 control-label">`;
+let colDiv = `<div class="col-sm-10">`;
+let input = `<input type="`
+let formControl = `class="form-control"`;
+let unDiv = `</div>
+</div>`
+let name = `${formGroup}
+${label}name" ${controlLabel}Name:</label>
+${colDiv}
+${input}text" ${formControl} id="name" name="user_name" placeholder="Your Name">
+${unDiv}`;
+let phone = `${formGroup}
+${label}phone" ${controlLabel}Phone:</label>
+${colDiv}
+${input}tel" ${formControl} id="phone" name="user_phone" placeholder="(###)###-####">
+${unDiv}`;
+let email = `${formGroup}
+${label}email" ${controlLabel}Email:</label>
+${colDiv}
+${input}mail" ${formControl} id="email" name="user_email" placeholder="you@example.com">
+${unDiv}`;
+let msg = `${formGroup}
+${label}msg" ${controlLabel}Message:</label>
+${colDiv}
+<textarea ${formControl} id="msg" name="user_message" placeholder="Max length 500 characters"></textarea>
+${unDiv}`;
+let button = `${formGroup}
+<div class="col-sm-offset-2 col-sm-10">
+<button type="submit" class="btn btn-lg btn-primary send">Send</button>
+${unDiv}`;
+let contactForm = `${form}
+${name}
+${phone}
+${email}
+${msg}
+${button}
+</form>`;
 
 $('.btn').click(function () {
-  $('.cover').append(contactForm)
+  $('body').append(contactForm)
 })
